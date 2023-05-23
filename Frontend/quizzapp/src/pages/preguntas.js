d4c74891c90ab5c99512f8e8e8fbfe32aedc91b0
Yranda <mfcr112000@gmail.com>
2023-05-23 16:04:14 -0600

    changes

---
+++ Frontend/quizzapp/src/pages/preguntas.js
@@ -0,0 +1,14 @@
+const Preguntas=()=>{
+    return(
+    <div>
+    <h1>Preguntas</h1>
+    <h2>Pregunta</h2>
+        <p>Display Question </p>
+        <form>
+            
+        </form>
+
+    </div>
+    );
+}
+export default Preguntas;
