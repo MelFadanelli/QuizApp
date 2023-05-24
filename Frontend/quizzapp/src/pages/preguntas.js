969a1168d829588d9d23da813b0753dc4ca02408
Yranda <mfcr112000@gmail.com>
2023-05-23 23:23:15 -0600

    mensajes

--- Frontend/quizzapp/src/pages/preguntas.js
+++ Frontend/quizzapp/src/pages/preguntas.js
@@ -5,19 +5,54 @@ import '../styless/card.css'
 const Preguntas=()=>{
     const [question, setQuestion] = useState(""); // Placeholder for question
     const [answers, setAnswers] = useState([]);
+    const [Answer, setAnswer] = useState("");
     const [correctAnswer, setCorrectAnswer]= useState("");
+    const [selectedAnswer, setSelectedAnswer] = useState("");
+    const [correctAnswersCount, setCorrectAnswersCount] = useState(0);
+   const [indexa, setIndexa]=useState(0);
 
-    useEffect(() => {
-   
-        setQuestion(questionsData[0].question);
-        setAnswers([...questionsData[0].wrongAnswers, questionsData[0].answer]);
-        setCorrectAnswer(questionsData[0].answer);
-      }, []);
+    useEffect(() => {  
+        setQuestion(questionsData[indexa].question);
+        setAnswers([...questionsData[indexa].wrongAnswers, questionsData[indexa].answer]);
+        setCorrectAnswer(questionsData[indexa].answer);
+        //setIndexa(questionsData[0].id);
+      }, [indexa]);
+
+      const handleAnswerChange = (e) => {
+        setSelectedAnswer(e.target.value);
+      };
+
+      const handIndexa=()=>{
+        console.log("AQUIIII ESTAA", indexa)
+      }
+
+
+
+
+     const handlesubmit=()=>{
+        if (selectedAnswer === correctAnswer) {
+            setCorrectAnswersCount((prevCount) => prevCount + 1);
+           
+      }
+      if (indexa+1 === questionsData.length) {
+        alert(`Your final score is ${correctAnswersCount}`);
+      }else{
+        setIndexa((prevCount)=>prevCount+1);
+        console.log(correctAnswersCount, indexa)
+      }
+
+      
+    }
+
+
+
+
 
 
 
     return(
         <>
+        <h3>Correct Answers: {correctAnswersCount}</h3>
         <div className="card">
             <div >
         <h1>Questions</h1>
@@ -28,19 +63,24 @@ const Preguntas=()=>{
               <input
                 type="radio"
                 value={answer}
-                checked={answer === correctAnswer}
-                onChange={(e) => setCorrectAnswer(e.target.value)}
+                checked={answer === selectedAnswer}
+                onChange={handleAnswerChange}
+                
               />
               <label>{answer}</label>
             </div>
           ))}
+       
         </form>
+        <p>num question: {indexa+1}</p>
         
       </div>
       
       </div>
-      <button className="btn">Next</button>
+      <button className="btn" onClick={handlesubmit}>Next</button>
+      <button className="btn" onClick={handIndexa}>GATO PELON</button>
       </>
     );
 }
+
 export default Preguntas;
